eeab3f2315e0d769e333e511e72ca7eb574d28ec
test1 <test1@test.com>
2019-03-02 09:40:28 +0530

    adding extent reports

--- src/test/java/OrgTest/SelTestClass.java
+++ src/test/java/OrgTest/SelTestClass.java
@@ -6,24 +6,67 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReporter;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
 public class SelTestClass {
 	WebDriver driver;
+	
+	ExtentHtmlReporter reporter = new ExtentHtmlReporter("C:\\Users\\giridhar\\workspace8\\DemoProj\\test-output\\ExtentReports\\report.html");
+	ExtentReports extent = new ExtentReports();
+    
+    
 	@BeforeClass
 	public void setUp(){
 		
 		String driverPath = "C:\\Users\\giridhar\\workspace4\\SeleniumTest\\DriverServer\\Chrome\\chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver", driverPath);
 		 driver = new ChromeDriver();
+		 driver.manage().window().maximize();
 		
 	}
 	
 	@Test
 	public void testA(){
 		
-		driver.get("http://www.google.com");
 		
+		
+		extent.attachReporter(reporter);	
+	    ExtentTest logger = extent.createTest("testA"); 
+	    
+	     
+	    driver.get("http://www.google.com");
+	    
+	   logger.log(Status.INFO, "login GMAIL info");
+	   logger.log(Status.PASS, "login pass");
 		//testing the files in the 
 		
+	   extent.flush();
+		//new line added
+		//another line added
+		//another line added to test jenkins
+		
+		
+	}
+	
+	@Test
+	public void testB(){
+		
+		
+		
+		ExtentTest logger = extent.createTest("testB"); 
+	    
+	     
+	    driver.get("http://www.yahoo.com");
+	    
+	   logger.log(Status.INFO, "login YAHOO info");
+	   logger.log(Status.PASS, "login pass");
+		//testing the files in the 
+		
+	   extent.flush();
 		//new line added
 		//another line added
 		//another line added to test jenkins
