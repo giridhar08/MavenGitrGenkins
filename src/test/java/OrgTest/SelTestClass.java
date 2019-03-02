24ea5d24d35724529e8dba0816143f6aaf014931
test1 <test1@test.com>
2019-03-02 15:54:09 +0530

    chnages made to add screenshot shot using extent report

--- src/test/java/OrgTest/SelTestClass.java
+++ src/test/java/OrgTest/SelTestClass.java
@@ -1,5 +1,13 @@
 package OrgTest;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import org.apache.commons.io.FileUtils;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -12,9 +20,12 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.utils.FileUtil;
 
 public class SelTestClass {
-	WebDriver driver;
+	static WebDriver driver;
+	
+	static String filePath ="C:\\Users\\giridhar\\workspace8\\DemoProj\\test-output\\ExtentReports\\test.png";
 	
 	ExtentHtmlReporter reporter = new ExtentHtmlReporter("C:\\Users\\giridhar\\workspace8\\DemoProj\\test-output\\ExtentReports\\report.html");
 	ExtentReports extent = new ExtentReports();
@@ -56,7 +67,7 @@ public class SelTestClass {
 	}
 	
 	@Test
-	public void testB(){
+	public void testB() throws IOException{
 		
 		
 		
@@ -68,6 +79,8 @@ public class SelTestClass {
 	   logger.log(Status.INFO, "login YAHOO info");
 	   logger.log(Status.PASS, "login pass");
 		//testing the files in the 
+	   
+	     logger.addScreenCaptureFromPath(SelTestClass.getScreenshot());
 		
 	   extent.flush();
 		//new line added
@@ -76,6 +89,22 @@ public class SelTestClass {
 		
 		
 	}
+	
+	
+	public static String  getScreenshot() throws IOException{
+		
+		TakesScreenshot src = (TakesScreenshot)driver;
+		
+		File source = src.getScreenshotAs(OutputType.FILE);
+		
+		File destination = new File(filePath);
+		FileUtils.copyFile(source, destination);
+		
+		
+		return filePath;
+		
+	}
+	
 	
 	@AfterClass
 	public void testDown(){
