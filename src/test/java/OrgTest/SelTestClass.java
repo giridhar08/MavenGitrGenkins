22511799a52a1693061125d4b23410e2b0e9cce2
test1 <test1@test.com>
2019-03-02 07:22:34 +0530

    commiting all the jaav project files

---
+++ src/test/java/OrgTest/SelTestClass.java
@@ -0,0 +1,35 @@
+package OrgTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class SelTestClass {
+	WebDriver driver;
+	@BeforeClass
+	public void setUp(){
+		
+		String driverPath = "C:\\Users\\giridhar\\workspace4\\SeleniumTest\\DriverServer\\Chrome\\chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", driverPath);
+		 driver = new ChromeDriver();
+		
+	}
+	
+	@Test
+	public void testA(){
+		
+		driver.get("http://www.google.com");
+		
+		
+	}
+	
+	@AfterClass
+	public void testDown(){
+		
+		driver.quit();
+		
+	}
+
+}
